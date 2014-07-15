12f88cb8a5d22b8ba215b75c52cc1fef6531d44b
Michael Bianco <iloveitaly@gmail.com>
2014-07-14 21:29:45 -0400

    Allow timeout, open_timeout, user, and password to be passed to rest-client

--- lib/rspec_api_test/http_helpers.rb
+++ lib/rspec_api_test/http_helpers.rb
@@ -30,11 +30,27 @@ class RSpecAPITest
     end
 
     def request(*args)
+      request_args = { method: args.first, url: args[1] }
+
       defaults = RSpecAPITest.config[:defaults] || {}
-      opts_i = args[2].is_a?(String) ? 3 : 2
-      args[opts_i] ||= {} if defaults
-      args[opts_i].reverse_merge!(defaults) 
-      RestClient.send(*args)
+
+      if args[2].is_a?(String)
+        request_args[:payload] = args[2]
+        opts_i = 3
+      else
+        opts_i = 2
+      end
+
+      args[opts_i] ||= {}
+      args[opts_i].reverse_merge!(defaults) if defaults
+
+      request_args[:timeout] = args[opts_i].delete(:timeout) if args[opts_i].has_key?(:timeout)
+      request_args[:open_timeout] = args[opts_i].delete(:open_timeout) if args[opts_i].has_key?(:open_timeout)
+      request_args[:user] = args[opts_i].delete(:user) if args[opts_i].has_key?(:user)
+      request_args[:password] = args[opts_i].delete(:password) if args[opts_i].has_key?(:password)
+      request_args[:headers] = args[opts_i]
+
+      RestClient::Request.execute(request_args)
     rescue RestClient::Exception => e
       e.response
     end
